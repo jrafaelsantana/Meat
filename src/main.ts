5091ac2565287f00ed249ca5776de1e34a670cb7
vanessabandeira <van3ssabandeira@gmail.com>
2019-10-30 21:49:02 -0300

    variable

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+declare var sprintf: any;
+
 
 if (environment.production) {
   enableProdMode();
+  console.log(sprintf("%s %s!", "Check", "Done!"));
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
